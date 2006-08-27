db5a2bc028f362bb5e1e827f40ec6734a6937562
sir-haleem <aleemisiaka@gmail.com>
2006-08-28 00:13:17 +0100

    Init

---
+++ assets/custom/js/custom.js
@@ -0,0 +1,17 @@
+( // Opens Execution Context
+    function (jQ) { // Begins a self invoking funtion
+
+
+
+
+
+
+        // CODE GOES IN HERE!!!
+
+
+
+
+
+
+    }(jQuery) // ends and also passes jQuery Object as parameter to a self invoking function
+); // Ends Created Execution Context
