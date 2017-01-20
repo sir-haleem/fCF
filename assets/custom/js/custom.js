5380e0052146dba9be0d594b3f49be82ce5aed41
sir-haleem <aleemisiaka@gmail.com>
2017-01-21 00:19:43 +0100

    more mods

--- assets/custom/js/custom.js
+++ assets/custom/js/custom.js
@@ -11,7 +11,36 @@
         on: 'click',
     });
 
-;
+    jQ('.ui.radio.checkbox').checkbox();
+    jQ('.ui.checkbox').checkbox();
+
+    console.log(jQ('.header#main-section'));
+
+    jQ('.header#main-section').click(function(evt){
+        var name = jQ(this).data('name')
+        var count = jQ(this).data('count');
+        var self = this;
+
+         jQ('.item#section[data-count="'+count+'"] .list').transition({
+            animation : 'fly left',
+            interval  : 200,
+            onComplete: function() {
+                console.log(count);
+                if(jQ(self).data('opened') === 'opened') {
+                    jQ('.item#section[data-count="'+count+'"] i').removeClass('open')
+                    jQ(self).html("<a>" + name +"</a>");
+                    jQ(self).data('opened', 'false')
+                } else{
+                    jQ(self).html(name);
+                    jQ('.item#section[data-count="'+count+'"] i').addClass('open')                    
+                    jQ(self).data('opened', 'opened')
+                    
+                }
+            }
+        });
+
+         
+    });
 
         
 
